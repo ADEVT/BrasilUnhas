d842f8fbd14ee81183206f2d58eb49e24a556e81
Augusto <Augusto@Smileinface>
2014-05-23 16:34:33 -0300

    criação do projeto

---
+++ src/br/adevt/brasilunhas/Principal.java
@@ -0,0 +1,15 @@
+package br.adevt.brasilunhas;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Principal extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_principal);
+    }
+}
