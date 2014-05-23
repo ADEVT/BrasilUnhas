482c0907e3ef5d6dd1875dfb8ba877334dcd5af8
Augusto <Augusto@Smileinface>
2014-05-23 17:10:53 -0300

    ads

--- src/br/adevt/brasilunhas/Principal.java
+++ src/br/adevt/brasilunhas/Principal.java
@@ -1,15 +1,52 @@
 package br.adevt.brasilunhas;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 public class Principal extends Activity {
+	private AdView adView;
+	private final String AD_UNIT_ID = "ca-app-pub-4698331571681120/9045635697";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.tela_principal);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tela_principal);
+
+		adView = new AdView(this);
+		adView.setAdUnitId(AD_UNIT_ID);
+		adView.setAdSize(AdSize.SMART_BANNER);
+		
+		LinearLayout layout = (LinearLayout) findViewById(R.id.banner);
+
+		layout.addView(adView);
+
+		AdRequest adRequest = new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR).build();
+
+		adView.loadAd(adRequest);
+	}
+
+	@Override
+	public void onPause() {
+		adView.pause();
+		super.onPause();
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		adView.resume();
+	}
+
+	@Override
+	public void onDestroy() {
+		adView.destroy();
+		super.onDestroy();
+	}
 }
